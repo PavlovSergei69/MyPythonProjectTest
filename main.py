53b98cd00798118c63492aa099635d17f6ae383a
PavlovSergei69 <serezhaunknown@gmail.com>
2025-02-12 16:12:06 +0300

    Сохранение модуля "Синтаксис языка"

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+from operator import truediv
+
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print_hi('Sergei')
+    number = 100  # integer
+    float_number = 16.5  # float
+    text = 'Цитата: "А внутри другая цитата"'  #string
+    another_text = 'Еще одна цитата'    #string
+    bolean_value = True                 #bolean
+    another_bolean_value = False        #bolean
+    a,b,c = 10,15,25 #переменные вложеннеы в одну строку
+    print(a) #выдает значение 10
+    values = 1, 'число', 43, 99         #это список!
+    #print(values[1])                   #выведет второе число. Отсчет начинается с 0. В данном варианте будет 'число'
+    dictionary = {"car": "машина", "dog":"собака", "cat":"кошка"}
+    #print(dictionary["car"])           #выведет значение "машина".
+    #print(number, float_number,bolean_value, sep=',') #Sep (разделитель) разделяет значение определенным знаком, который задан разработчиком
+
