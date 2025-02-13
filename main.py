892eaf23c44f6e0dd8527d444a8e32c524142c88
PavlovSergei69 <serezhaunknown@gmail.com>
2025-02-13 15:45:19 +0300

    Добавление нового файла 'SET and Frozenset. Удаление файла test.txt

--- main.py
+++ main.py
@@ -13,18 +13,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print_hi('Sergei')
-    number = 100  # integer
-    float_number = 16.5  # float
-    text = 'Цитата: "А внутри другая цитата"'  #string
-    another_text = 'Еще одна цитата'    #string
-    bolean_value = True                 #bolean
-    another_bolean_value = False        #bolean
-    a,b,c = 10,15,25 #переменные вложеннеы в одну строку
-    print(a) #выдает значение 10
-    values = 1, 'число', 43, 99         #это список!
-    #print(values[1])                   #выведет второе число. Отсчет начинается с 0. В данном варианте будет 'число'
-    dictionary = {"car": "машина", "dog":"собака", "cat":"кошка"}
-    #print(dictionary["car"])           #выведет значение "машина".
-    #print(number, float_number,bolean_value, sep=',') #Sep (разделитель) разделяет значение определенным знаком, который задан разработчиком
+    print_hi('Sergei')
+
 
